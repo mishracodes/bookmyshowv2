1110ffae99bd37a056ebb672d518ee0ef7ef60c7
Amit Kumar Mishra <31859346+mishracodes@users.noreply.github.com>
2023-03-26 18:04:07 +0530

    makemyshow intial commit. added header and crousel

---
+++ components/Crousal/Crousal.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+const Crousal = () => {
+    return (
+        <Carousel showStatus={false} showIndicators={false} showThumbs={false} infiniteLoop={true} autoPlay={true} interval={2000}>
+            <div>
+                <img  src="https://assets-in.bmscdn.com/promotions/cms/creatives/1678276688384_bbsweb.jpg" />
+            </div>
+            <div>
+                <img loading='lazy' src="https://assets-in.bmscdn.com/promotions/cms/creatives/1678860245525_borisweb.jpg" />
+            </div>
+            <div>
+                <img loading='lazy' src="https://assets-in.bmscdn.com/promotions/cms/creatives/1679337190446_honeyweb.jpg" />
+            </div>
+            <div>
+                <img loading='lazy' src="https://assets-in.bmscdn.com/promotions/cms/creatives/1679647614140_offerweb.jpg" />
+            </div>
+            
+        </Carousel>
+    )
+}
+
+export default Crousal
